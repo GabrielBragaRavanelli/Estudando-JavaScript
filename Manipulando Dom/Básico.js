7e37c88c1340f436038b79bb8d728507914fa9b4
Gabriel Braga Ravanelli <gabrielravanelli1@gmail.com>
2026-04-29 17:59:13 -0300

    Aula do projeto Fokus - Manipulando o DOM

--- Manipulando Dom/Básico.js
+++ Manipulando Dom/Básico.js
@@ -11,3 +11,32 @@ const curtobt = document.getElementById("curtobtn");
 curtobt.addEventListener("click", () => {
     // Função que quero executar quando clicar no botão
 });
+
+// O setinterval é uma função que executa uma função a cada intervalo de tempo especificado. Por exemplo, se quisermos atualizar o conteúdo de um elemento a cada segundo, podemos usar o setInterval da seguinte maneira:
+setInterval(() => {
+    // Código para atualizar o conteúdo do elemento
+}, 1000);
+
+//clearInterval é usado para parar a execução de uma função que foi iniciada com setInterval. Por exemplo, se quisermos parar a atualização do conteúdo do elemento após 10 segundos, podemos usar clearInterval da seguinte maneira:
+const intervalo = setInterval(() => {
+    // Código para atualizar o conteúdo do elemento
+}, 1000);
+
+//new date() - para pegar a data atual
+const dataAtual = new Date();
+console.log(dataAtual);
+
+// Para modificar o texto de um elemento, podemos usar a propriedade `textContent` ou `innerText`. Por exemplo:
+meuElemento.textContent = "Novo texto para o elemento";
+
+// O innerHTML é usado para modificar o conteúdo HTML de um elemento. Por exemplo:
+meuElemento.innerHTML = "<strong>Texto em negrito</strong>";
+
+// O setattribute é usado para definir um atributo em um elemento. Por exemplo, para definir o atributo "src" de uma imagem:
+const minhaImagem = document.getElementById("minha-imagem");
+minhaImagem.setAttribute("src", "caminho/para/nova-imagem.jpg");
+
+//musica.loop = true - para deixar a musica em loop
+const musica = document.getElementById("minha-musica");
+musica.loop = true;
+
