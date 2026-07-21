541b30afa9fe6bcf90b7aaaedae56b7cc04a17f2
Gabriel Braga Ravanelli <gabrielravanelli1@gmail.com>
2026-07-21 14:14:05 -0300

    Aprendizados

--- escolha.js
+++ escolha.js
@@ -28,3 +28,66 @@ switch(notadoAluno){
 //pode traduzir switch com escolha.
 
 
+const nota1 = 6
+const nota2 = 7
+const nota3 = 5
+const nota4 = 9
+
+const media = (nota1 + nota2 + nota3 + nota4) / 4
+
+switch(media){
+    case 10:
+    case 9:
+        console.log("Excelente nota")
+        break
+    case 8:
+    case 7:
+        console.log("Boa nota")
+    case 6:
+        console.log ("Nota suficiente")
+        break
+    case 5:
+        console.log("Aluno realizará Exame")
+        break
+    case 4:
+    case 3:
+    case 2:
+    case 1:
+    case 0:
+        console.log("Reprovado")
+        break
+    default:
+        console.log("Nota inválida")
+    
+}
+
+const camisa = 80*3
+const short = 60 *2
+const cueca = 40*6
+const calca = 70
+
+const mediacompra = (camisa + short + cueca + calca) /12
+console.log(mediacompranode)
+
+switch(mediacompra){
+    case 150:
+    case 120:
+        console.log("Saiu caro.")
+        break
+    case 100:
+    case 80:
+        console.log("Saiu barato.")
+    case 70:
+    case 60:
+    case 50:
+    case 40:
+        console.log("Dinheiro muito bem gasto.")
+        break
+    case 20:
+    case 10:
+        default:
+        console.log("De graça.")
+}
+
+
+
